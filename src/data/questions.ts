47d7623541b783b1c736209d5c4f5e775e84b25d
AbraaoDev <abraaohomualdo@gmail.com>
2025-02-07 16:24:25 -0300

    :sweet_potato: Done strutcture

---
+++ src/data/questions.ts
@@ -0,0 +1,14 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Qual é a capital da França?",
+    options: ["Paris", "Londres", "Berlim", "Roma"],
+    answer: "Paris",
+  },
+  {
+    id: 2,
+    question: "Quanto é 2 + 2?",
+    options: ["3", "4", "5", "6"],
+    answer: "4",
+  },
+];
