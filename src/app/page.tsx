7f2257a1397db6b47eeb412d9d0b97040ecde0d8
AbraaoDev <abraaohomualdo@gmail.com>
2025-02-07 16:40:52 -0300

    :sweet_potato: Screen tablet and Questions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="max-w-[1216px] mx-auto px-6">
+    <div className="max-w-[1280px] mx-auto px-6">
       <h1 className="text-lg font-bold">Quiz Carnavalense</h1>
     </div>
   );
--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,14 +1,62 @@
 export const questions = [
   {
     id: 1,
-    question: "Qual é a capital da França?",
-    options: ["Paris", "Londres", "Berlim", "Roma"],
-    answer: "Paris",
+    question: "Qual a primeira marchinha de carnaval?",
+    options: ["Sassaricando", "Mamãe eu quero", "Allah-lá-ô", "Ô abre alas"],
+    answer: "Ô abre alas"
   },
   {
     id: 2,
-    question: "Quanto é 2 + 2?",
-    options: ["3", "4", "5", "6"],
-    answer: "4",
+    question: "Quem foi o (a) compositor (a) da primeira marchinha?",
+    options: ["Lamartine Babo", "Chiquinha Gonzaga", "Ary Barroso", "Braguinha"],
+    answer: "Chiquinha Gonzaga"
   },
+  {
+    id: 3,
+    question: "Em que ano foi composta a primeira marchinha?",
+    options: ["1910", "1900", "1899", "1916"],
+    answer: "1899"
+  },
+  {
+    id: 4,
+    question: "Qual a menor máscara do mundo?",
+    options: ["Lágrima", "Nariz de palhaço", "Luvas", "Peruca"],
+    answer: "Nariz de palhaço"
+  },
+  {
+    id: 5,
+    question: "O que significa o corso no carnaval?",
+    options: ["Uma fantasia", "Um tipo de dança", "Grupo de fantasiados", "Desfiles de carros ornamentados com foliões fantasiados"],
+    answer: "Desfiles de carros ornamentados com foliões fantasiados"
+  },
+  {
+    id: 6,
+    question: "Como surgiu o confete?",
+    options: ["Surgiu de uma fábrica de papel", "Surgiu no carnaval em Roma em forma de confeitos", "Surgiu nos carnavais de rua", "O confete de papel surgiu em Paris em 1892", "Alternativas a e c", "Alternativas b e d"],
+    answer: "Alternativas b e d"
+  },
+  {
+    id: 7,
+    question: "Como começaram a jogar serpentina?",
+    options: ["Pelas ruas", "Nos clubes nos antigos bailes", "Nas janelas dos prédios", "Nos corsos"],
+    answer: "Nas janelas dos prédios"
+  },
+  {
+    id: 8,
+    question: "Qual marchinha foi gravada por Moacyr Franco?",
+    options: ["Saca-rolha", "Cidade maravilhosa", "Me dá um dinheiro aí", "Turma do funil"],
+    answer: "Me dá um dinheiro aí"
+  },
+  {
+    id: 9,
+    question: "Quem foi o compositor da marchinha 'A jardineira'?",
+    options: ["Lamartine Babo", "Humberto Porto", "Ary Barroso", "Braguinha"],
+    answer: "Humberto Porto"
+  },
+  {
+    id: 10,
+    question: "Quem gravou a marchinha 'Bandeira branca'?",
+    options: ["Marlene", "Dalva de Oliveira", "Emilinha Borba", "Isaurinha Garcia"],
+    answer: "Dalva de Oliveira"
+  }
 ];
